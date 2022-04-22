d46196fc589a99ccb834a569ee9285601ec4a1c2
Mike Kaminski <mike@mkny.net>
2022-04-21 22:39:03 -0400

    implement binary search

--- btree/btree_test.go
+++ btree/btree_test.go
@@ -24,3 +24,88 @@ func TestBTree(t *testing.T) {
 		t.Errorf("page result was not the same page that was inserted")
 	}
 }
+
+func TestBinarySearch(t *testing.T) {
+
+	tbl := []struct {
+		needle uint16
+		elems  []uint16
+		offset int
+		found  bool
+	}{
+		{
+			needle: 5,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 2,
+			found:  true,
+		},
+		{
+			needle: 3,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 1,
+			found:  true,
+		},
+		{
+			needle: 7,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 3,
+			found:  true,
+		},
+		{
+			needle: 1,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 0,
+			found:  true,
+		},
+		{
+			needle: 9,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 4,
+			found:  true,
+		},
+		{
+			needle: 0,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 0,
+			found:  false,
+		},
+		{
+			needle: 2,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 1,
+			found:  false,
+		},
+		{
+			needle: 4,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 2,
+			found:  false,
+		},
+		{
+			needle: 6,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 3,
+			found:  false,
+		},
+		{
+			needle: 8,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 4,
+			found:  false,
+		},
+		{
+			needle: 10,
+			elems:  []uint16{1, 3, 5, 7, 9},
+			offset: 5,
+			found:  false,
+		},
+	}
+
+	for _, v := range tbl {
+		offset, found := binarySearch(v.needle, v.elems)
+		if offset != v.offset || found != v.found {
+			t.Errorf("[needle]: %d [elems]: %v [expected offset]: %d [actual offset]: %d [expected found]: %t [actual found]: %t",
+				v.needle, v.elems, v.offset, offset, v.found, found)
+		}
+	}
+}
