ed054c2e3f9f04cf2eba7abb7a5cd5bf7cec96bd
Mike <mk6i@proton.me>
2022-05-03 22:49:25 -0400

    set up btree to insert and read from single page

--- storage/btree_test.go
+++ storage/btree_test.go
@@ -1,26 +1,43 @@
 package btree
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestBTree(t *testing.T) {
 
 	bt := &BTree{&memoryStore{}}
+	bt.append(&page{
+		cellType: KeyValueCell,
+	})
 
-	pg := &page{}
-
-	offset, err := bt.insert(pg)
-	if err != nil {
-		t.Errorf("failed to insert %s", err.Error())
+	tbl := []struct {
+		key uint32
+		val []byte
+	}{
+		{key: 1, val: []byte("hello")},
+		{key: 2, val: []byte("bounjour")},
+		{key: 3, val: []byte("hallo")},
 	}
 
-	res, err := bt.find(offset)
-	if err != nil {
-		t.Errorf("failed to find %s", err.Error())
+	for _, expect := range tbl {
+		key, err := bt.insert(expect.val)
+		if err != nil {
+			t.Errorf("got insertion error for %s: %s", expect.val, err.Error())
+		}
+		if key != expect.key {
+			t.Errorf("expected primary key %d, got %d", uint32(expect.key), key)
+		}
 	}
 
-	if res == nil {
-		t.Errorf("did not find page that was inserted")
-	} else if res != pg {
-		t.Errorf("page result was not the same page that was inserted")
+	for _, expect := range tbl {
+		val, err := bt.find(expect.key)
+		if err != nil {
+			t.Errorf("got retrieval error for %d: %s", expect.key, err.Error())
+		}
+		if !bytes.Equal(val, expect.val) {
+			t.Errorf("value mismatch for key %d. got %s, expected %s", expect.key, val, expect.val)
+		}
 	}
 }
