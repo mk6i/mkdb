cc70764604b74fe950e7b902420dc719f36f2a84
Mike <mk6i@proton.me>
2022-06-24 10:49:25 -0400

    support non-monotonically increasing id insertion

--- storage/btree_test.go
+++ storage/btree_test.go
@@ -24,21 +24,21 @@ func TestBTree(t *testing.T) {
 		key uint32
 		val []byte
 	}{
-		{key: 1, val: []byte("hello")},
 		{key: 2, val: []byte("bounjour")},
-		{key: 3, val: []byte("hallo")},
-		{key: 4, val: []byte("chat")},
+		{key: 1, val: []byte("hello")},
 		{key: 5, val: []byte("chien")},
-		{key: 6, val: []byte("hammer")},
+		{key: 11, val: []byte("lumber")},
+		{key: 4, val: []byte("chat")},
+		{key: 10, val: []byte("saw")},
 		{key: 7, val: []byte("nail")},
 		{key: 8, val: []byte("screwdriver")},
 		{key: 9, val: []byte("screw")},
-		{key: 10, val: []byte("saw")},
-		{key: 11, val: []byte("lumber")},
+		{key: 3, val: []byte("hallo")},
+		{key: 6, val: []byte("hammer")},
 	}
 
 	for _, expect := range tbl {
-		key, err := bt.insert(expect.val)
+		key, err := bt.insertKey(expect.key, expect.val)
 		if err != nil {
 			t.Errorf("got insertion error for %s: %s", expect.val, err.Error())
 		}
@@ -57,10 +57,30 @@ func TestBTree(t *testing.T) {
 		}
 	}
 
+	tblSorted := []struct {
+		key uint32
+		val []byte
+	}{
+		{key: 1, val: []byte("hello")},
+		{key: 2, val: []byte("bounjour")},
+		{key: 3, val: []byte("hallo")},
+		{key: 4, val: []byte("chat")},
+		{key: 5, val: []byte("chien")},
+		{key: 6, val: []byte("hammer")},
+		{key: 7, val: []byte("nail")},
+		{key: 8, val: []byte("screwdriver")},
+		{key: 9, val: []byte("screw")},
+		{key: 10, val: []byte("saw")},
+		{key: 11, val: []byte("lumber")},
+	}
+
 	ch := bt.scanRight()
 
-	for _, expect := range tbl {
-		val := <-ch
+	for _, expect := range tblSorted {
+		val, ok := <-ch
+		if !ok {
+			t.Fatalf("channel is unexpectedly empty")
+		}
 		if !bytes.Equal(val.valueBytes, expect.val) {
 			t.Errorf("value mismatch for key %d. got %s, expected %s", expect.key, val.valueBytes, expect.val)
 		}
@@ -68,10 +88,13 @@ func TestBTree(t *testing.T) {
 
 	ch = bt.scanLeft()
 
-	for i := len(tbl) - 1; i >= 0; i-- {
-		val := <-ch
-		if !bytes.Equal(val.valueBytes, tbl[i].val) {
-			t.Errorf("value mismatch")
+	for i := len(tblSorted) - 1; i >= 0; i-- {
+		val, ok := <-ch
+		if !ok {
+			t.Fatalf("channel is unexpectedly empty")
+		}
+		if !bytes.Equal(val.valueBytes, tblSorted[i].val) {
+			t.Errorf("value mismatch for key %d. got %s, expected %s", tblSorted[i].key, val.valueBytes, tblSorted[i].val)
 		}
 	}
 }
