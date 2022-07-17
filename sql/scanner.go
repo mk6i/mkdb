9b0be83245e27e7b5dbc50d769dcfc3c8c89420a
Mike Kaminski <mike@mkny.net>
2022-07-17 13:12:35 -0400

    implement sql scanner

---
+++ sql/scanner.go
@@ -0,0 +1,194 @@
+package sql
+
+import (
+	"io"
+	"text/scanner"
+)
+
+const (
+	EOF = iota
+
+	literal_start
+	IDENT
+	INT
+	STR
+	literal_end
+
+	operator_start
+	AND
+	OR
+	ASTRSK
+	EQ
+	GT
+	LT
+	LTE
+	GTE
+	LPAREN
+	RPAREN
+	operator_end
+
+	keyword_start
+	AS
+	BEGIN
+	BY
+	CASE
+	COMMIT
+	COUNT
+	DISTINCT
+	ELSE
+	END
+	EXISTS
+	FALSE
+	FROM
+	FULL
+	GROUP
+	HAVING
+	IN
+	JOIN
+	LEFT
+	LIKE
+	MAX
+	MIN
+	NOT
+	NULL
+	ON
+	ORDER
+	OUTER
+	SELECT
+	SUM
+	THEN
+	TRUE
+	UNION
+	UNIQUE
+	WHEN
+	WHERE
+	WITH
+	keyword_end
+)
+
+type TokenType int
+
+func (t TokenType) IsKeyword() bool {
+	return keyword_start < t && t < keyword_end
+}
+
+func (t TokenType) IsLiteral() bool {
+	return literal_start < t && t < literal_end
+}
+
+func (t TokenType) IsOperator() bool {
+	return operator_start < t && t < operator_end
+}
+
+var Tokens = map[TokenType]string{
+	EOF: "EOF",
+
+	IDENT: "IDENT",
+	INT:   "INT",
+	STR:   "STR",
+
+	AND:    "AND",
+	OR:     "OR",
+	ASTRSK: "*",
+	EQ:     "=",
+	GT:     ">",
+	LT:     "<",
+	LTE:    "<=",
+	GTE:    ">=",
+	LPAREN: "(",
+	RPAREN: ")",
+
+	AS:       "AS",
+	BEGIN:    "BEGIN",
+	BY:       "BY",
+	CASE:     "CASE",
+	COMMIT:   "COMMIT",
+	COUNT:    "COUNT",
+	DISTINCT: "DISTINCT",
+	ELSE:     "ELSE",
+	END:      "END",
+	EXISTS:   "EXISTS",
+	FALSE:    "FALSE",
+	FROM:     "FROM",
+	FULL:     "FULL",
+	GROUP:    "GROUP",
+	HAVING:   "HAVING",
+	IN:       "IN",
+	JOIN:     "JOIN",
+	LEFT:     "LEFT",
+	LIKE:     "LIKE",
+	MAX:      "MAX",
+	MIN:      "MIN",
+	NOT:      "NOT",
+	NULL:     "NULL",
+	ON:       "ON",
+	ORDER:    "ORDER",
+	OUTER:    "OUTER",
+	SELECT:   "SELECT",
+	SUM:      "SUM",
+	THEN:     "THEN",
+	TRUE:     "TRUE",
+	UNION:    "UNION",
+	UNIQUE:   "UNIQUE",
+	WHEN:     "WHEN",
+	WHERE:    "WHERE",
+	WITH:     "WITH",
+}
+
+var keywords map[string]TokenType
+
+func init() {
+	keywords = make(map[string]TokenType)
+	for i := TokenType(operator_start) + 1; i < operator_end; i++ {
+		keywords[Tokens[i]] = i
+	}
+	for i := TokenType(keyword_start) + 1; i < keyword_end; i++ {
+		keywords[Tokens[i]] = i
+	}
+}
+
+type Token struct {
+	Type   TokenType
+	Line   int
+	Column int
+	Text   string
+}
+
+type tokenScanner struct {
+	s   scanner.Scanner
+	cur rune
+}
+
+func NewTokenScanner(src io.Reader) *tokenScanner {
+	ts := &tokenScanner{}
+	ts.s.Init(src)
+	return ts
+}
+
+func (ts *tokenScanner) Cur() Token {
+	tok := Token{
+		Column: ts.s.Column,
+		Line:   ts.s.Line,
+		Text:   ts.s.TokenText(),
+	}
+	switch ts.cur {
+	case scanner.EOF:
+		tok.Type = EOF
+	case scanner.Ident:
+		tok.Type = IDENT
+	case scanner.Int:
+		tok.Type = INT
+	default:
+		if kw, isKw := keywords[tok.Text]; isKw {
+			tok.Type = kw
+		} else {
+			tok.Type = STR
+		}
+	}
+	return tok
+}
+
+func (ts *tokenScanner) Next() bool {
+	ts.cur = ts.s.Scan()
+	return ts.cur != scanner.EOF
+}
