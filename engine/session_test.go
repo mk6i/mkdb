f5932937c663cb059df1d26010ca65ef778abc24
Mike Kaminski <mike@mkny.net>
2022-08-04 14:00:37 -0400

    implement console

---
+++ engine/session_test.go
@@ -0,0 +1,78 @@
+package engine
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+
+	defer func() {
+		if err := os.Remove("data/testdb"); err != nil {
+			t.Logf("error removing db: %s", err.Error())
+		}
+	}()
+
+	queries := []string{
+		`CREATE DATABASE testdb`,
+		`USE testdb`,
+		`CREATE TABLE people (
+			person_id int,
+			first_name varchar(255),
+			last_name varchar(255)
+		)`,
+		`CREATE TABLE cars (
+			name varchar(255)
+		)`,
+		`CREATE TABLE planes (
+			name varchar(255)
+		)`,
+		`CREATE TABLE boats (
+			name varchar(255)
+		)`,
+		`CREATE TABLE trains (
+			name varchar(255)
+		)`,
+		`CREATE TABLE helicopters (
+			name varchar(255)
+		)`,
+		`CREATE TABLE bicycles (
+			name varchar(255)
+		)`,
+		`CREATE TABLE motorcycles (
+			name varchar(255)
+		)`,
+		`CREATE TABLE minivans (
+			name varchar(255)
+		)`,
+		`CREATE TABLE scooters (
+			name varchar(255)
+		)`,
+		`CREATE TABLE rollerblades (
+			name varchar(255)
+		)`,
+		`CREATE TABLE tram (
+			name varchar(255)
+		)`,
+		`SELECT table_name, page_id FROM sys_pages`,
+		`SELECT table_name, field_name, field_length, field_type FROM sys_schema`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (1, "John", "Doe")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (2, "Ikra", "Freeman")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (3, "Gerrard", "Torres")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (4, "Malia", "Brewer")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (5, "Willow", "Reeves")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (6, "Mylee", "Mclean")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (7, "Leland", "Booth")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (8, "Chance", "Snyder")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (9, "Cairo", "Lim")`,
+		`INSERT INTO people (person_id, first_name, last_name) VALUES (10, "Khadija", "Crane")`,
+		`SELECT person_id, first_name, last_name FROM people`,
+	}
+
+	s := Session{}
+	for _, q := range queries {
+		if err := s.ExecQuery(q); err != nil {
+			t.Errorf("error running query:\n %s\nError: %s", q, err.Error())
+		}
+	}
+}
